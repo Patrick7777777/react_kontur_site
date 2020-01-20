458b5a86a576ecfbbf618f9702afe416da3318bc
Евгений Киреев <ekireev@live.com>
2020-01-21 00:46:04 +0300

    отслеживание ширины экрана и передача ее в KontMenu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import KontHeader from './components/KontHeader';
+import KontPicture from './components/KontPicture';
+import KontLogo from './components/KontLogo';
+import KontFooter from './components/KontFooter';
+import KontInfo from './components/KontInfo';
+import KontArticle from './components/KontArticle';
+import KontMenu from './components/KontMenu';
+import {BrowserRouter} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            height: window.innerHeight,
+            width: window.innerWidth
+        };
+        this.updateDimensions = this.updateDimensions.bind(this);
+    }
+
+    componentDidMount() {
+        window.addEventListener("resize", this.updateDimensions);
+    }
+
+    updateDimensions() {
+        this.setState({
+            height: window.innerHeight,
+            width: window.innerWidth
+        });
+        console.log(this.state.height);
+        console.log(this.state.width)
+    }
+
+
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div className='KontWrapper'>
+                    <KontHeader/>
+                    <KontLogo/>
+                    <KontMenu xxx = {this.state.width}/>
+                    <KontPicture/>
+                    <KontArticle/>
+                    <KontFooter/>
+                    <KontInfo/>
+                </div>
+            </BrowserRouter>
+        );
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.updateDimensions);
+    }
 }
 
 export default App;
